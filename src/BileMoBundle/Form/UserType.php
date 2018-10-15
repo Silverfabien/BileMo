5b2b118f657ab618a4c674a8f237ec8a0aaddb8e
fabien hollebeque <hollebeque.fabien@hotmail.com>
2018-10-15 17:05:23 +0200

    Ajout du formulaire de connexion

--- src/BileMoBundle/Form/UserType.php
+++ src/BileMoBundle/Form/UserType.php
@@ -3,6 +3,9 @@
 namespace BileMoBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,14 +16,15 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('username')->add('password')->add('apiKey');
+        $builder->add('username', TextType::class, ['label' => 'Pseudo:', 'required' => true])
+                ->add('password', PasswordType::class, ['label' => 'Mot de passe:', 'required' => true])
+                ->add('_remember_me', CheckboxType::class, ['required' => false]);
     }/**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'BileMoBundle\Entity\User'
         ));
     }
 
