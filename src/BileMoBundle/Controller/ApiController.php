b513e07bae851f4b656caf1731e2642ae5670267
fabien hollebeque <hollebeque.fabien@hotmail.com>
2018-10-16 16:35:34 +0200

    Changement des nom de route et ajout de l'inscription

--- src/BileMoBundle/Controller/ApiController.php
+++ src/BileMoBundle/Controller/ApiController.php
@@ -2,21 +2,24 @@
 
 namespace BileMoBundle\Controller;
 
+use BileMoBundle\Entity\User;
 use BileMoBundle\Exception\ResourceValidationException;
+use BileMoBundle\Form\LoginType;
 use BileMoBundle\Form\UserType;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\ConstraintViolationList;
 
 class ApiController extends Controller
 {
     /**
-     * @Route("/api/token", name="connexion_api_page")
+     * @Route("/api/token", name="bile_mo_api_token")
      */
     public function newTokenAction(UserInterface $user, JWTTokenManagerInterface $jwtManager)
     {
@@ -24,21 +27,22 @@ class ApiController extends Controller
     }
 
     /**
-     * @Route("/users", name="connexion_page")
+     * @Route("/login", name="bile_mo_login_page")
      */
     public function loginAction()
     {
-        $loginForm = $this->createForm(UserType::class, ['username' => $this->get('security.authentication_utils')->getLastUsername()]);
+        $loginForm = $this->createForm(LoginType::class, ['username' => $this->get('security.authentication_utils')->getLastUsername()]);
 
         return $this->render('@BileMo/Default/login.html.twig', ['loginForm' => $loginForm->createView()]);
     }
 
     /**
-     * @Route("/logout", name="deconnexion_page")
+     * @Route("/logout", name="bile_mo_logout")
      * @throws \Exception
      */
     public function logoutAction()
     {
+        $this->redirectToRoute('bile_mo_homepage');
         throw new \Exception('This should not be reached !');
     }
 
@@ -57,5 +61,26 @@ class ApiController extends Controller
 
             throw new ResourceValidationException($message);
         }
+    }
+
+    /**
+     * @Route("/register", name="bile_mo_register_page")
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $formRegister = $this->getDoctrine()->getManager();
+            $formRegister->persist($user);
+            $formRegister->flush();
+
+            return $this->redirectToRoute('bile_mo_login_page', ['id' => $user->getId()]);
+        }
+
+        return $this->render('@BileMo/Default/register.html.twig', ['user' => $user, 'registerForm' => $form->createView()]);
     }
 }
